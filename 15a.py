122613d56a2e70c8b86320abeaabdcfb23b22489
Fabio Arciniegas <fabio_arciniegas@trendmicro.com>
2022-12-15 02:30:36 -0800

    day 15

--- 15a.py
+++ 15a.py
@@ -9,7 +9,7 @@ import cv2
 
 
 f = open("input_15.txt")
-f = open("input_15_sample.txt")
+#f = open("input_15_sample.txt")
 
 cart = np.zeros((900, 900), dtype=int)
 
@@ -18,7 +18,7 @@ coords = "Sensor at x=(\-?\d+), y=(-?\d+): closest beacon is at x=(\-?\d+), y=(\
 def manhattan_distance(x1,y1,x2,y2):
     return abs(x1-x2) + abs(y1-y2)
 
-row = 10
+row = 2000000
 def von_neumman_neighborhood_cutting_points(x,y,r,row):
     # given the von neumman neighborhood centered in x1,y1 with radius r
     # and a given row, return the numbers of the columns that intersect the edge
@@ -36,7 +36,7 @@ def von_neumman_neighborhood_cutting_points(x,y,r,row):
         return None,None
 
     x1 = x - abs(r - abs(y-row))
-    x2 = x + abs(r - abs(y-row)) 
+    x2 = x + abs(r - abs(y-row))
     return (x1, x2)
 
 cuts = von_neumman_neighborhood_cutting_points
@@ -57,30 +57,36 @@ discarded = []
 for region in known:
     segment = cuts(region[0],region[1],region[2],row)
     if segment[0]:
-        discarded.append(segment[0])
-        discarded.append(segment[1])
+        discarded.append((segment[0],segment[1]))
 
-discarded.sort()
-print(discarded)
+discarded = sorted(discarded)
+
+def overlap(s1,s2):
+    if s2[0] <= s1[1] and s2[1] <= s1[1]: # contained
+        return -1,s1
+    if s2[0] <= s1[1] and s2[1] > s1[1]: # extended
+        return 0,(s1[0],s2[1])
+    if s2[0] > s1[1]: # no overlap
+        return 1,s2
+
+
+curs = discarded[0]
+non_overlapping = []
+for t in discarded[1:]:
+    res,rescurs = overlap(curs,t)
+    if res == 1:
+        non_overlapping.append(curs)
+        curs = t
+    if res == 0:
+        curs = rescurs
+    if res == -1:
+        continue
+
+non_overlapping.append(curs)
 
 count = 0
-rightmost = -math.inf
-continuous = False
-for i in range(len(discarded)):
-    print("rightmost:",rightmost,"i:",discarded[i],"count:",count)
-    if i % 2 == 1:
-        count += discarded[i]-rightmost + 1
-        rightmost = discarded[i] 
-    else:
-        if discarded[i] == rightmost:
-            count -= 1
-        else:
-            rightmost = discarded[i]
-            
-#5892813 high
+for i in non_overlapping:
+    count += i[1]-i[0]
 
-#5367847
-#5367846 too low
 print(count)
 
-print(cuts(0,7,3,10))
---
+++ 15b.py
@@ -0,0 +1,106 @@
+import numpy as np
+import math
+import re
+from PIL import Image
+import cv2
+
+
+
+
+
+
+f = open("input_15_sample.txt")
+f = open("input_15.txt")
+
+cart = np.zeros((900, 900), dtype=int)
+
+coords = "Sensor at x=(\-?\d+), y=(-?\d+): closest beacon is at x=(\-?\d+), y=(\-?\d+)"
+
+def manhattan_distance(x1,y1,x2,y2):
+    return abs(x1-x2) + abs(y1-y2)
+
+row = 2000000
+def von_neumman_neighborhood_cutting_points(x,y,r,row):
+    # given the von neumman neighborhood centered in x1,y1 with radius r
+    # and a given row, return the numbers of the columns that intersect the edge
+    # of the neighborhood in that row
+
+    if abs(row-y) > r:
+        # no intersection, row outside of the neighborhood altogether
+        return None,None
+
+    x1 = x - abs(r - abs(y-row))
+    x2 = x + abs(r - abs(y-row))
+    return (x1, x2)
+
+def overlap(s1,s2):
+    if s2[0] <= s1[1] and s2[1] <= s1[1]: # contained
+        return -1,s1
+    if s2[0] <= s1[1] and s2[1] > s1[1]: # extended
+        return 0,(s1[0],s2[1])
+    if s2[0] > s1[1]: # no overlap
+        return 1,s2
+
+
+    
+cuts = von_neumman_neighborhood_cutting_points
+
+known = []
+for l in f:
+    m = re.match(coords, l)
+    sx = int(m.group(1))
+    sy = int(m.group(2))
+    bx = int(m.group(3))
+    by = int(m.group(4))
+    d = manhattan_distance(sx,sy,bx,by)
+#    print(sx,sy,bx,by,d)
+    known.append([sx,sy,d])
+
+    
+
+discarded = []
+
+for row in range(0,4000000):
+    all_in_row = []
+    discarded = []
+    for beacon in known:
+        segment = cuts(beacon[0],beacon[1],beacon[2],row)
+        if segment[0]:
+            discarded.append((segment[0],segment[1]))
+    if not discarded:
+        continue
+    discarded = sorted(discarded)
+    
+    curs = discarded[0]
+    non_overlapping = []
+    for t in discarded[1:]:
+        res,rescurs = overlap(curs,t)
+        if res == 1:
+            non_overlapping.append(curs)
+            curs = t
+        if res == 0:
+            curs = rescurs
+        if res == -1:
+            continue
+    non_overlapping.append(curs)
+
+    #--- got all overlapping for this row across all beacons
+    
+    # if len(non_overlapping) > 1:
+    #     print(len(non_overlapping))
+    #     print("more than one non overlapping in row",row)
+    #     print("namely:",non_overlapping)
+
+    if len(non_overlapping) == 2:
+        if non_overlapping[1][0] - non_overlapping[0][1] == 2:
+            print("x:",non_overlapping[1][0]-1,"y:",row)
+            print((non_overlapping[1][0]-1) * 4000000 + row)
+#    all_in_row.append(non_overlapping[0])
+#    if len(set(all_in_row))==2:
+#print(row,"all discarded:",discarded)
+#        print(row,"segments seen by all beacons in row",set(all_in_row))
+    
+#    for i in non_overlapping:
+#        count += i[1]-i[0]
+#    print("sanity count for row:",row,":",count)
+
