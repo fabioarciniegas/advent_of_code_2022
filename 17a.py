7edd35f2866ea6738e0f57b7c43b4eb5832f77c1
Fabio Arciniegas <fabio_arciniegas@trendmicro.com>
2022-12-17 02:13:57 -0800

    starting at right places

--- 17a.py
+++ 17a.py
@@ -14,7 +14,7 @@ np.set_printoptions(threshold=np.inf, linewidth=1000)
 f = open("input_17.txt")
 f = open("input_17_sample.txt")
 
-tetris = np.zeros((1, 7), dtype=int)
+tetris = np.zeros((30, 7), dtype=int)
 
 jets = f.readline()
 
@@ -57,75 +57,31 @@ def print_tetris(tetris):
     print("---------")
 
 
-def shift(tetris, amount=1, x=0):
-    l = tetris[x]
-    ones = np.where(l == 1)[0]
-    if amount == 1 and ones[-1] < len(l) - 1:
-        l = np.roll(l, amount)
-    if amount == -1 and ones[0] > 0:
-        l = np.roll(l, amount)
-    tetris[x, :] = l
-
-
-def leftmost(tetris, x, height):
-    lefts = []
-    for i in range(height):
-        l = tetris[x + i]
-        lefts.append(np.where(l == 1)[0][0])
-    return min(lefts)
-
-
-def rightmost(tetris, x, height):
-    rights = []
-    for i in range(height):
-        l = tetris[x + i]
-        rights.append(np.where(l == 1)[0][-1])
-    return max(rights)
-
-
-def collides_next(tetris, top, height):
-    if top + height >= len(tetris):
-        return True
-    for i in range(height - 1, -1, -1):
-        addition = np.add(tetris[top + i], tetris[top + i + 1])
-        print(addition)
-        if np.where(addition == 2)[0].size > 0:
-            return True
-    return False
-
-
-#    addition = np.add(tetris[bottomx],tetris[bottomx+1])
-#    return np.where(addition == 2)[0].size > 0
-
-
-def drop(tetris, top, height):
-    print(top, height)
-    for i in range(height - 1, -1, -1):
-        tetris[top + i + 1, :] = np.add(tetris[top + i], tetris[top + i + 1])
-        tetris[top + i, :] = [0, 0, 0, 0, 0, 0, 0]
-
 
 n = 0
 while n < 10:
     jet = -1
     highest = 0
     for l in tetris:
-        print(l)
+        highest +=1
         if np.any(l):
             print(np.any(l))
             break
-        highest +=1
-    
-    tetris = expand_tetris(tetris,n=3-highest)
+    print(highest)
+
     shape = shapes[n % len(shapes)]
     left = 1
-    top = 0
     width = 0
     for each_line in shape:
         if len(each_line) > width:
             width = len(each_line)
-    height = put(tetris, shape, top, left)
-    print("new")
+    height = len(shape)
+
+#    tetris = expand_tetris(tetris,-(highest-3-height))
+    top = highest-1-3-height
+    put(tetris, shape, top, left)
+
+    print_tetris(tetris)
     while top + height < len(tetris):  #not collides_next(tetris,top,height):
         jet += 1
         if jet % 2 == 1:
@@ -155,7 +111,7 @@ while n < 10:
                 remove(tetris, shape, top, left)
                 put(tetris, shape, top - 1,left)
                 break
-        print_tetris(tetris)
-        time.sleep(0.5)
-        os.system('clear')
+
+#        time.sleep(0.5)
+#        os.system('clear')
     n += 1
