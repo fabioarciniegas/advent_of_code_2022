3301d19f33edbefb976f6f142d253cea70c5c5ca
Fabio Arciniegas <fabio_arciniegas@trendmicro.com>
2022-12-23 13:47:17 -0800

    read input

--- 22.py
+++ 22.py
@@ -58,6 +58,28 @@ def print_board(board):
         print()
 
 
+def move(inst,board,pos,dir):
+    if inst == "L":
+        return (dir - 1)%4
+    if inst == "R":
+        return (dir + 1)%4
+    if dir == 1:
+        move_right(inst,board)
+
+def move_right(inst,board,pos):
+    tiles = int(inst)
+    while tiles:
+        if board[pos[0][[pos[1]+1] == -1 or (pos[1]+1 == len(board[pos[0]]) and board[pos[0]][0] == -1):
+            return pos
+        if board[pos[0]][pos[1]+1] >= 0:
+            pos[1] +=1
+        if pos[1]+1 == len(board[pos[0]]):
+            pos[1] = 0
+        tiles -= 1
+        
+
+    
+
 filename = ""
 if len(sys.argv) > 1:
     filename = sys.argv[1]
@@ -75,12 +97,12 @@ instructions = re.findall(move,instructions_as_line)
 
 # You begin the path in the leftmost open tile of the top row of tiles.
 pos = [0,np.where(board[0] == 1)[0][0]]
-dir = 1 #1 right, 2 down, 3 left, 4 up
+dir = 0 #0 right, 1 down, 2 left, 3 up
 
 board[pos[0],pos[1]] = 2
 
 print_board(board)
 
 
-
-exit(2)
+for inst in instructions:
+    dir = move(inst,board,pos,dir)
