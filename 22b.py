423ae27062d792d52c841660710c4f8a65c5917e
Fabio Arciniegas <fabio_arciniegas@trendmicro.com>
2022-12-24 12:33:23 -0800

    start visualization

--- 22b.py
+++ 22b.py
@@ -67,215 +67,209 @@ def print_board(board):
                 print(board[i][j], end="")
         print()
 
+
 def face(pos):
     for f in tls:
-#        D(f"{f=} {tls[f]=} {pos=}")
-        if pos[0] >= tls[f][0] and pos[0] <= tls[f][0]+49 and pos[1] >= tls[f][1] and pos[1] <= tls[f][1]+49:
+        #        D(f"{f=} {tls[f]=} {pos=}")
+        if pos[0] >= tls[f][0] and pos[0] <= tls[f][0] + 49 and pos[1] >= tls[
+                f][1] and pos[1] <= tls[f][1] + 49:
             return f
     return None
 
 
-def move(inst, board, pos, direction):
-    if inst == "L":
-        return (direction - 1) % 4
-    if inst == "R":
-        return (direction + 1) % 4
-    if direction == 0:
-        D("right")
-        move_right(inst, board, pos)
-    if direction == 1:
-        D("going down")
-        move_down(inst, board, pos)
-    if direction == 2:
-        D("left")
-        move_left(inst, board, pos)
-    if direction == 3:
-        D("up")
-        move_up(inst, board, pos)
-    return direction
+def rightmost(pos):
+    return tls[face(pos)][1] + 49
 
+def topmost(pos):
+    return tls[face(pos)][0]
 
-def rightmost(pos):  
-    return tls[face(pos)][1]+49
-
-def topmost(pos):  
-    return tls[face(pos)][0]    
 
 def bottommost(pos):
     return tls[face(pos)][0] + 49
 
-def leftmost(pos):  
+
+def leftmost(pos):
     return tls[face(pos)][1]
 
 
 def targetright(pos):
-    target = [None,None]
-    next_face = { "A" : "B", "B" : "D", "C" : "B",
-                  "D" : "B", "E" : "D", "F" : "D" }
-    #0 right, 1 down, 2 left, 3 up
-    next_dir = { "A" : 0, "B" : 2, "C" : 3,"D" : 2, "E" : 0, "F" : 3 }
+    target = [None, None]
+    next_face = {"A": "B", "B": "D", "C": "B", "D": "B", "E": "D", "F": "D"}
+    # 0 right, 1 down, 2 left, 3 up
+    next_dir = {"A": 0, "B": 2, "C": 3, "D": 2, "E": 0, "F": 3}
 
     f = face(pos)
     nf = next_face[f]
     nd = next_dir[f]
     D(f"{pos=} {f=}, {nf=} {nd=}")
-    
+
     if nd == 0:
         target[0] = pos[0]
         target[1] = tls[nf][1]
     if nd == 2:
-        target[0] = topmost(tls[nf])+(bottommost(tls[f]) - pos[0])
+        target[0] = topmost(tls[nf]) + (bottommost(tls[f]) - pos[0])
         target[1] = rightmost(tls[nf])
     if nd == 3:
         target[0] = bottommost(tls[nf])
-        target[1] = leftmost(tls[nf]) + (pos[0]-topmost(tls[f]))
-    return target,nd
+        target[1] = leftmost(tls[nf]) + (pos[0] - topmost(tls[f]))
+    return target, nd
 
 
 def targetleft(pos):
     # only good for edges
-    target = [None,None]
-    next_face = { "A" : "E", "B" : "A", "C" : "E",
-                  "D" : "E", "E" : "A", "F" : "A" }
-    #0 right, 1 down, 2 left, 3 up
-    next_dir = { "A" : 0, "B" : 2, "C" : 1,"D" : 2, "E" : 0, "F" : 1 }
+    target = [None, None]
+    next_face = {"A": "E", "B": "A", "C": "E", "D": "E", "E": "A", "F": "A"}
+    next_dir = {"A": 0, "B": 2, "C": 1, "D": 2, "E": 0, "F": 1}
 
     f = face(pos)
     nf = next_face[f]
     nd = next_dir[f]
     D(f"{pos=} {f=}, {nf=} {nd=}")
-    
+
     if nd == 0:
-        target[0] = bottommost(tls[nf])-(pos[0]-topmost(tls[f]))
+        target[0] = bottommost(tls[nf]) - (pos[0] - topmost(tls[f]))
         target[1] = leftmost(tls[nf])
     if nd == 2:
         target[0] = pos[0]
         target[1] = rightmost(tls[nf])
     if nd == 1:
         target[0] = topmost(tls[nf])
-        target[1] = leftmost(tls[nf]) + (pos[0]-topmost(tls[f]))
-    return target,nd
+        target[1] = leftmost(tls[nf]) + (pos[0] - topmost(tls[f]))
+    return target, nd
 
 
 def targetdown(pos):
     # only good for edges
-    target = [None,None]
-    next_face = { "A" : "C", "B" : "C", "C" : "D",
-                  "D" : "F", "E" : "F", "F" : "B" }
-    #0 right, 1 down, 2 left, 3 up
-    next_dir = { "A" : 1, "B" : 2, "C" : 1,"D" : 2, "E" : 1, "F" : 1 }
+    target = [None, None]
+    next_face = {"A": "C", "B": "C", "C": "D", "D": "F", "E": "F", "F": "B"}
+    next_dir = {"A": 1, "B": 2, "C": 1, "D": 2, "E": 1, "F": 1}
 
     f = face(pos)
     nf = next_face[f]
     nd = next_dir[f]
     D(f"{pos=} {f=}, {nf=} {nd=}")
-    
+
     if nd == 1:
         target[0] = topmost(tls[nf])
         target[1] = pos[1]
     if nd == 2:
-        target[0] = topmost(tls[nf])+(pos[1]-leftmost(tls[f]))
+        target[0] = topmost(tls[nf]) + (pos[1] - leftmost(tls[f]))
         target[1] = rightmost(tls[nf])
-    return target,nd
-
-
-
+    return target, nd
 
 
 def targetup(pos):
     # only good for edges
-    target = [None,None]
-    next_face = { "A" : "F", "B" : "F", "C" : "A",
-                  "D" : "C", "E" : "C", "F" : "E" }
-    #0 right, 1 down, 2 left, 3 up
-    next_dir = { "A" : 0, "B" : 3, "C" : 3,"D" : 3, "E" : 0, "F" : 3 }
+    target = [None, None]
+    next_face = {"A": "F", "B": "F", "C": "A", "D": "C", "E": "C", "F": "E"}
+    next_dir = {"A": 0, "B": 3, "C": 3, "D": 3, "E": 0, "F": 3}
 
     f = face(pos)
     nf = next_face[f]
     nd = next_dir[f]
     D(f"{f=}, {nf=} {nd=}")
-    
+
     if nd == 0:
-        target[0] = topmost(tls[nf])+(pos[1]-leftmost(tls[f]))
+        target[0] = topmost(tls[nf]) + (pos[1] - leftmost(tls[f]))
         target[1] = leftmost(tls[nf])
     if nd == 3:
         target[0] = bottommost(tls[nf])
-        target[1] = leftmost(tls[nf])+(pos[1]-leftmost(tls[f]))
-    return target,nd
+        target[1] = leftmost(tls[nf]) + (pos[1] - leftmost(tls[f]))
+    return target, nd
 
 
-           
+def manual_test_cases():
+    # coarse minimal testing
+    D(targetright((49, 99)))
+    D(targetright((49, 149)))
+    D(targetright((50, 99)))
+    D(targetright((99, 99)))
+    D(targetright((160, 49)))
+    D(targetleft((0, 50)))
+    ops = targetleft, targetdown, targetright, targetup
+    for corner in tls.values():
+        for op in ops:
+            print(f"{corner} using {op.__name__} : {op(corner)}")
+
+
 def move_right(inst, board, pos):
     tiles = int(inst)
     R = rightmost(pos)
-    L = leftmost(pos)
-#    D(f"{R=}")
-#    D(f"{L=}")
     while tiles:
         if pos[1] < R and board[pos[0]][pos[1] + 1] == 1:
             pos[1] += 1
         elif pos[1] == R:
             target, nd = targetright(pos)
             if board[target[0]][target[1]] == 1:
-                pos[0],pos[1] = target[0], target[1]
-            return tiles-1, nd
+                pos[0], pos[1] = target[0], target[1]
+            return tiles - 1, nd
         tiles -= 1
     return tiles, 0
 
 
 def move_down(inst, board, pos):
     tiles = int(inst)
-    T = topmost(board, pos)
-    B = bottommost(board, pos)
-    D(f"{T=}")
-    D(f"{B=}")
+    B = bottommost(pos)
     while tiles:
         if pos[0] < B and board[pos[0] + 1][pos[1]] == 1:
             pos[0] += 1
-#            board[pos[0]][pos[1]] = 3
-        elif pos[0] == B and board[T][pos[1]] == 1:
-            D(f"{pos=}")
-            D(f"{tiles=}")
-            pos[0] = T
-
-
-#            board[pos[0]][pos[1]] = 3
+        elif pos[0] == B:
+            target, nd = targetdown(pos)
+            if board[target[0]][target[1]] == 1:
+                pos[0], pos[1] = target[0], target[1]
+            return tiles - 1, nd
         tiles -= 1
+    return tiles, 0
 
 
 def move_left(inst, board, pos):
     tiles = int(inst)
-    R = rightmost(board, pos[0])
-    L = leftmost(board, pos[0])
-    D(f"{R=}")
-    D(f"{L=}")
-
+    L = leftmost(pos)
     while tiles:
         if pos[1] > L and board[pos[0]][pos[1] - 1] == 1:
             pos[1] -= 1
-#            board[pos[0]][pos[1]] = 3
-        elif pos[1] == L and board[pos[0]][R] == 1:
-            pos[1] = R
-
-
-#            board[pos[0]][pos[1]] = 3
+        elif pos[1] == L:
+            target, nd = targetleft(pos)
+            if board[target[0]][target[1]] == 1:
+                pos[0], pos[1] = target[0], target[1]
+            return tiles - 1, nd
         tiles -= 1
+    return tiles, 0
 
 
 def move_up(inst, board, pos):
     tiles = int(inst)
-    T = topmost(board, pos[1])
-    B = bottommost(board, pos[1])
+    T = topmost(pos)
     while tiles:
         if pos[0] > T and board[pos[0] - 1][pos[1]] == 1:
             pos[0] -= 1
-#            board[pos[0]][pos[1]] = 3
-        elif pos[0] == T and board[B][pos[1]] == 1:
-            pos[0] = B
-
-
-#            board[pos[0]][pos[1]] = 3
+        elif pos[0] == T:
+            target, nd = targetup(pos)
+            if board[target[0]][target[1]] == 1:
+                pos[0], pos[1] = target[0], target[1]
+            return tiles - 1, nd
         tiles -= 1
+    return tiles, 0
+
+
+def move(inst, board, pos, direction):
+    if inst == "L":
+        return (direction - 1) % 4
+    elif inst == "R":
+        return (direction + 1) % 4
+    
+    tiles = int(inst)
+    while tiles:
+        if direction == 0:
+            tiles,direction = move_right(tiles, board, pos)
+        if direction == 1:
+            tiles,direction = move_down(tiles, board, pos)
+        if direction == 2:
+            tiles,direction = move_left(tiles, board, pos)
+        if direction == 3:
+            tiles,direction = move_up(tiles, board, pos)
+    return direction
+
 
 
 def password(board, pos, direction):
@@ -301,48 +295,10 @@ instructions = re.findall(move_regexp, instructions_as_line)
 pos = [0, np.where(board[0] == 1)[0][0]]
 direction = 0  #0 right, 1 down, 2 left, 3 up
 
-#board[pos[0], pos[1]] = 3
-
-# test right movement
-D(targetright((49,99)))
-D(targetright((49,149)))
-D(targetright((50,99)))
-D(targetright((99,99)))
-D(targetright((160,49)))
-
-
-D("LEFT")
-# test left movement
-D(targetleft((0,50)))
-
-# pos = [0,97]
-# more = 50
-# while more:
-#     more,d = move_right(more,board,pos)
-#     D(f"{pos=},{more=},{d=}")
-# D(f"{pos=}")    
-
-ops = targetleft,targetdown,targetright,targetup
-
-for corner in tls.values():
-    for op in ops:
-        print(f"{corner} using {op.__name__} : {op(corner)}")
-
-
-
-
-
-
-
-exit(2)
-D(board.shape)
-
 for inst in instructions:
     D(f"{pos=}{direction=}")
     direction = move(inst, board, pos, direction)
-    #print_board(board)
-    #print()
-D("-==----------")
-#print_board(board)
 
 print(f"{password(board,pos,direction)=}")
+
+exit(0)
