f6d40c829a9f0ce815cadaac843877f7d0e8bd93
Fabio Arciniegas <fabio_arciniegas@trendmicro.com>
2022-12-26 19:28:38 -0800

    do tsp first as prep

--- tsp.py
+++ tsp.py
@@ -45,110 +45,12 @@ V = get_value
 def set_value(G, label, value):
     G.nodes[label]['node_attr']['flow'] = value
 
-
-def max_with_budget(i, labels, solutions, floyd, budget, current):
-    D(f"{budget=}")
-    max_value = solutions[i][budget - 1]
-
-    while budget:
-        candidates = [
-            cl for cl in labels
-            if floyd[current][cl] <= budget and floyd[current][cl] != 0
-        ]
-        D(f"Candidates from {current=} are {candidates=}")
-        for candidate in candidates:
-            max_candidate_value = 0
-            value = 0
-            candidate_index = items.index(candidate)
-            d = floyd[current][candidate]
-            D(f"{current=} {candidate=} {candidate_index=}, {budget=} {d=}")
-            value = solutions[candidate_index][
-                budget -
-                d]  #+ max_with_budget(candidate_index-1,labels,solutions,floyd,budget-d,candidate)
-            if value > max_candidate_value:
-                max_candidate = candidate_index
-                max_budget_decrease = d
-                max_candidate_value = value
-
-        D(f"{max_value=}")
-    return max_value
-
-
-def max_w_budget2(i, b, labels, solutions, floyd, seen=[], skip_first=False):
-    option_1 = solutions[i][b - 1]
-    #max that I can reach in d steps plus what I can reach from there in another b-d steps
-    possible_maxes = []
-    possible_maxes.append(option_1 if not skip_first else 0)
-    current_label = labels[i]
-    D(f"{i=} {b=} {seen=}")
-    exclude = seen.copy()
-
-    for cl in [x for x in labels if x not in seen]:
-        cl_index = labels.index(cl)
-        d = floyd[current_label][cl]
-        if d <= b and d > 0:
-            exclude.append(current_label)
-            possible_maxes.append(solutions[i][b - d] + solutions[cl_index][d])
-
-
-#                                  max_w_budget2(cl_index, b - d,labels,solutions,floyd,exclude,True))
-        D(possible_maxes)
-        D(solutions)
-    return max(possible_maxes)
-
-
-def max_flow(capacity, labels, G, from_node, floyd):
-    solutions = np.zeros((len(labels), capacity + 1))
-    #    items = [g for g in G.nodes]
-    # base case of i=0. IF we were not using np.zeros would have to be explicit:
-    #    for c in range(capacity + 1):
-    #        solutions[0][c] = 0
-    seen = []
-    for i in range(len(labels)):
-        solutions[i][1] = get_value(G, labels[i])
-
-    for i in range(0, solutions.shape[0]):
-        for c in range(2, solutions.shape[1]):
-            solutions[i][c] = max_w_budget2(i, c, labels, solutions, floyd,
-                                            seen)
-
-    D(solutions)
-    # for c in range(capacity+1):
-    #     for i in range(len(labels)):
-    #         d = floyd["AA"][labels[i]]
-    #         if d <= c:
-    #             solutions[1][c] = max(solutions[1][c],get_value(G,labels[i]))
-
-    return 2, solutions
-
-
-#     last_visited = "AA"
-
-#     for i in range(1, len(labels)+1):
-#         cur_label = labels[i-1]
-#         for c in range(capacity+1):
-#             s_i = floyd["AA"][cur_label] + 1
-#             v_i = get_value(G,cur_label)
-# #            D(f"{s_i=}{v_i=}")
-
-#             if s_i  > c:
-#                 solutions[i][c] = solutions[i-1][c]!
-#             else:
-#                 solutions[i][c] = max(solutions[i-1][c],
-#                                       solutions[i-1][c - s_i] + v_i*(capacity - s_i - c))
-
-#     return solutions[len(items)][capacity], solutions
-
-
-#def findOptimalTour(floyd,solutions,s,n):
-    
-
 def findMinCost(m,solutions,s,n,names):
     # end state is bit mask with N bits sets to 1 (2**n - 1)
     end_state = (1 << n) - 1
-
     mintourcost = math.inf
     for e in range(n):
+        D(f"{e=} {end_state=} {solutions[e][end_state]=}")
         if e == s:
             continue
         tourcost = solutions[e][end_state] + m[e][s]
@@ -180,7 +82,7 @@ def notIn(s,subset):
     return ((1 << s) & subset) == 0
 
 def solve(m, solutions, s, n,names):
-    for r in range(3, n):
+    for r in range(3, n+1):
         for subset in combinations(r, n):
             if notIn(s,subset):
                 continue
@@ -193,9 +95,9 @@ def solve(m, solutions, s, n,names):
                     if e == s or e == next_s or notIn(e,subset):
                         continue
                     newDist = solutions[e][subset_wo_next] + m[e][next_s]
-                    D(f"considering {names[e]=} and {names[next_s]=} with {newDist=}")
                     minDist=min(minDist,newDist)
-                    
+
+                D(f" {next_s=}  {subset=} with {newDist=}")
                 solutions[next_s][subset] = minDist
 
 
@@ -253,7 +155,7 @@ minCost = findMinCost(m, solutions,s,N,node_names_ordered)
 
 n = len(node_names_ordered)
 
-D(solutions)
+#D(solutions)
 D(node_names_ordered)
 D(m)
 D(f"{minCost=}")
